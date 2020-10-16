3288e309a6e0cb51e797199db7bee7dea6eb105c
Khafiz <hafizbatyrkhan@gmail.com>
2020-10-17 01:06:57 +0600

    Token Authentication added. Note that encryption is not set yet.

--- src/main/java/dreamteam/hotelchainproject/repositories/UserRepository.java
+++ src/main/java/dreamteam/hotelchainproject/repositories/UserRepository.java
@@ -3,6 +3,13 @@ package dreamteam.hotelchainproject.repositories;
 import dreamteam.hotelchainproject.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
     User findUserById(Long id);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
 }
