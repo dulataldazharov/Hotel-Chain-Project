7818da162d931ff9565600f5e8f19ba1244979f8
Khafiz <hafizbatyrkhan@gmail.com>
2020-11-22 13:16:35 +0600

    hotel employee list BACK-12

--- src/main/java/dreamteam/hotelchainproject/repositories/HotelRepository.java
+++ src/main/java/dreamteam/hotelchainproject/repositories/HotelRepository.java
@@ -4,4 +4,5 @@ import dreamteam.hotelchainproject.models.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HotelRepository extends JpaRepository<Hotel,Integer> {
+    Hotel getById(int hotelId);
 }
