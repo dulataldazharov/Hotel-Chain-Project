b7582f7724191b7d0e0d1806f2081cffa1948dcc
Khafiz <hafizbatyrkhan@gmail.com>
2020-11-22 11:23:09 +0600

    hotel employee list BACK-12

--- src/main/java/dreamteam/hotelchainproject/controllers/UserController.java
+++ src/main/java/dreamteam/hotelchainproject/controllers/UserController.java
@@ -1,14 +1,22 @@
 package dreamteam.hotelchainproject.controllers;
 
 import dreamteam.hotelchainproject.dto.UserInfoDto;
+import dreamteam.hotelchainproject.dto.employee.EmployeeDto;
+import dreamteam.hotelchainproject.models.Employee;
+import dreamteam.hotelchainproject.repositories.EmployeeRespository;
 import dreamteam.hotelchainproject.services.UserService;
+import dreamteam.hotelchainproject.servicesImpl.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +27,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private EmployeeRespository employeeRespository;
+    @Autowired
+    private EmployeeServiceImpl employeeServiceImpl;
 
     @GetMapping("/user/profile/personal")
     public UserInfoDto getUserPersonalInfo(){
@@ -26,4 +38,13 @@ public class UserController {
         String username = authentication.getName();
         return userService.getUserInfo(username);
     }
+    @GetMapping("/employee/list")
+    public ResponseEntity<List<EmployeeDto>> getEmployeeList(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Employee employee = employeeRespository.findById(username).orElse(null);
+        if(employee == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        return ResponseEntity.ok(employeeServiceImpl.getEmployeeDtosByManagerEmail(username));
+    }
+
 }
---
+++ src/main/java/dreamteam/hotelchainproject/servicesImpl/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package dreamteam.hotelchainproject.servicesImpl;
+
+import dreamteam.hotelchainproject.dto.employee.EmployeeDto;
+import dreamteam.hotelchainproject.models.Employee;
+import dreamteam.hotelchainproject.repositories.EmployeeRespository;
+import dreamteam.hotelchainproject.repositories.UserRepository;
+import dreamteam.hotelchainproject.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRespository employeeRespository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<EmployeeDto> getEmployeeDtosByManagerEmail(String email) {
+        List<EmployeeDto> result = new ArrayList<>();
+        Employee employee = employeeRespository.findById(email).get();
+        int hotelId = employee.getHotelId();
+        List<Employee> employees = employeeRespository.getAllByHotelId(hotelId);
+        for(Employee e: employees) {
+            result.add(mapEmployeetoDto(employee));
+        }
+        return result;
+    }
+    private EmployeeDto mapEmployeetoDto(Employee employee) {
+        EmployeeDto dto = new EmployeeDto();
+        dto.setUsername(employee.getEmail());
+        dto.setType(employee.getType());
+        return dto;
+    }
+}
